2c27031b4e55eb3f641af7afe35866ae7f9817a0
Stanley Thepkaisone <stanleythepkaisone@gmail.com>
2020-10-15 16:10:25 -0700

    Added and, or, xor, compliment and zerofill shift

--- Primitives.java
+++ Primitives.java
@@ -6,6 +6,11 @@ class primitives{
         System.out.println(countBits(15));
         System.out.println(shiftRight(15));
         System.out.println(shiftLeft(15));
+        System.out.println(bitwiseAnd(15, 2));
+        System.out.println(bitwiseOr(15,16));
+        System.out.println(bitwiseXOR(8, 1));
+        System.out.println(bitwiseCompliment(60));
+        System.out.println(zeroFillRightShift(16));
     }
 /**
  * 
@@ -39,21 +44,58 @@ public static int shiftLeft(int x){
     x <<= 2;
     return x;
 }
-/** 
-public static int bitwiseAnd(int x){
-    
+ /**
+  * takes 2 ints x, y. If bits exists in both they will be combined.
+  * ex. 1100 & 0100 = 0100
+  * @param x
+  * @param y
+  * @return int "and" of the new value
+  */
+public static int bitwiseAnd(int x, int y){
+    int and = x & y;
+    return and;
 }
-public static int bitwiseOr(int x){
-    
+/**
+ * takes 2 ints x, y. If bits exists in one or the other they will be combined.
+ * ex. 1100 | 0101 = 1101
+ * @param x
+ * @param y
+ * @return int "or" of the new value
+ */
+public static int bitwiseOr(int x, int y){
+    int or = x | y;
+    return or;
 }
-public static int bitwiseXOR(int x){
-    
+/**
+ * takes 2 ints x, y. If bits exists in one and not the other they will be combined.
+ * ex. 1100 ^ 0011 = 1111
+ * @param x
+ * @param y
+ * @return int "xor" of the new value
+ */
+public static int bitwiseXOR(int x, int y){
+    int xor = x ^ y;
+    return xor;
 }
+/**
+ * provides the compliment to the int also flips the sign
+ * ex. ~1100 = 0011
+ * @param x
+ * @return int compliment
+ */
 public static int bitwiseCompliment(int x){
-    
+    int compliment = ~x;
+    return compliment;
 }
+/**
+ * shifts bits over to the right by 2
+ * Fills shifted spots with 0's
+ * @param x
+ * @return zeroFill
+ */
 public static int zeroFillRightShift(int x){
-    
+    int zeroFill = x>>>2;
+    return zeroFill; 
 }
-*/
+
 }
